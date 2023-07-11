ca85e7d351bff6c7724a257d44fd5d79f1f32d4a
rpverson01 <rpverson01@tecmilenio.mx>
2023-07-10 22:03:44 -0600

    Server update for Render.com

--- server.js
+++ server.js
@@ -1,18 +1,35 @@
+//New Code
+
 const express = require('express');
 const app = express();
 const csv = require('csvtojson');
+const https = require('https');
 
 const csvFileUrl = 'https://github.com/rpverson/API/blob/main/data.csv'; // Reemplaza con la URL real de tu archivo CSV
+const port = process.env.PORT || 3000;
 
 app.get('/api/productos', (req, res) => {
-  csv()
-    .fromStream(request.get(csvFileUrl))
-    .then((jsonObj) => {
-      res.json(jsonObj);
+  https.get(csvFileUrl, (response) => {
+    let data = '';
+
+    response.on('data', (chunk) => {
+      data += chunk;
     });
+
+    response.on('end', () => {
+      csv()
+        .fromString(data)
+        .then((jsonObj) => {
+          res.json(jsonObj);
+        })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).json({ error: 'Internal server error' });
+        });
+    });
+  });
 });
 
-app.listen(3000, () => {
-  console.log('API running on port 3000');
+app.listen(port, () => {
+  console.log(`API running on port ${port}`);
 });
-
